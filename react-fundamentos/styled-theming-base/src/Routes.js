b7a556b8e8d62157009572a6909c35f8a0442188
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-06-24 13:16:38 -0300

    criando rotas

---
+++ react-fundamentos/styled-theming-base/src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+
+export default function Routes() {
+  return (
+    
+      <Route path='/' component={Home} />
+    
+  )
+}
